e27ca9d46c76f2defc9e81d4ee7ade614b305f89
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-02-22 08:32:51 +0100

    02-01-setter-newfile projekt

---
+++ 02-01-setter-newfile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02_01_setter_newfile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
