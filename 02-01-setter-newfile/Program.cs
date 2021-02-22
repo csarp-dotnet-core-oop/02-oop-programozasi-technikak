78418b5a156af26e79f3624e91729f7e55efe57e
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-02-22 08:35:36 +0100

    S1.01 Feladat szövege. Model mappa jelentősége.

--- 02-01-setter-newfile/Program.cs
+++ 02-01-setter-newfile/Program.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿// S1.01
+// A feladat szövege:
+// Vektorgrafikus rendszerekben lehetőség van négyzet rajzolására.
+// Az ilyen négyzetek méretét lehetséges változtatni.
+// Az ilyen CAD rendszereknél fontos lehet a négyzet területének és kerületének meghatározása.
+// Készítsen olyan négyzet osztályt amelynek adott az oldalhossza!
+// Az oldalhossz lekérhető és módosítható tulajdonsága legyen a négyzetnek!
+// A négyzet területe és kerülete legyen lekérdezhető tulajdonsága a négyzetnek!
+// Példányosítson egy ilyen négyzetet és a főprogramban mutassa be a tulajdonságok kezelését!
+// Az osztály a projekt model rétegében készítse el egy külön fájlba!
+
+using System;
 
 namespace _02_01_setter_newfile
 {
@@ -6,7 +17,8 @@ namespace _02_01_setter_newfile
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // S1.01 Készítsünk egy model mappát a projektünkben.
+            //       Abba hozzunk létre egy osztályt Square néven.
         }
     }
 }
