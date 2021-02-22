11ed7b7486be9d108da4a202cee4f42a9a9e7d74
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-02-22 08:50:47 +0100

    S1.11 A megírt kódok tesztelése.

--- 02-01-setter-newfile/Program.cs
+++ 02-01-setter-newfile/Program.cs
@@ -22,6 +22,34 @@ namespace _02_01_setter_newfile
         {
             // S1.01 Készítsünk egy model mappát a projektünkben.
             //       Abba hozzunk létre egy osztályt Square néven.
+
+            // S1.11 A megírt kódok tesztelése.
+            // A négyzet oldala 2
+            Square mySquare = new Square(2);
+            Console.WriteLine(mySquare);
+            Console.WriteLine("Kerülete: " + mySquare.District + " méter.");
+            Console.WriteLine("Területe: " + mySquare.Area + " négyzetméter.");
+
+            // A négyzet oldalát módosítani lehet
+            mySquare.Side = 5;
+
+            Console.WriteLine(mySquare);
+            Console.WriteLine("Kerülete: " + mySquare.District + " méter.");
+            Console.WriteLine("Területe: " + mySquare.Area + " négyzetméter.");
+
+            // A konyhába és a fürdőszobába szeretnénk padló csempét
+            Square myKitchen = new Square(4);
+            Square myBathroom = new Square(5);
+            // A négyzet oldalát módosítani lehet. Legyen nagyobb a konyha.
+            myKitchen.Side = 6;
+            double areaToBeTiled = myKitchen.Area + myBathroom.Area;
+
+            Console.WriteLine();
+            Console.WriteLine("Konyha, " + myKitchen);
+            Console.WriteLine("Konyha területe: " + myKitchen.Area + " négyzetméter.");
+            Console.WriteLine("Fürdőszoba, " + myBathroom);
+            Console.WriteLine("Fürdőszoba területe: " + myBathroom.Area + " négyzetméter.");
+            Console.WriteLine("Csempézve lesz: " + areaToBeTiled + " négyzetméter.");
         }
     }
 }
