6ad28e961be657d295bb8e024afbbccadad48f9a
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-02-22 08:37:26 +0100

    S1.02 Sqare osztály a model rétegben és a Square.cs állományban.

---
+++ 02-01-setter-newfile/model/Square.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02_01_setter_newfile.model
+{
+    /// <summary>
+    /// S1.02 Sqare osztály a model rétegben és a Square.cs állományban.
+    ///       Figyeljük meg, hogy a namespace most "_04SquareProject.model" lett.
+    /// </summary>
+    class Square
+    {
+    }
+}
