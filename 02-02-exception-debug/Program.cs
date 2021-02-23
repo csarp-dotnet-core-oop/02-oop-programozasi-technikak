03f7b0fa3ce05787ce43122bd8c26dc40c575756
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-02-23 12:40:08 +0100

    02-02-exception-debug projekt

---
+++ 02-02-exception-debug/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02_02_exception_debug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
