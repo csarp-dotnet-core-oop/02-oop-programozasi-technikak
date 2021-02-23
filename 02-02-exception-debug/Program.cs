bb24bcff6dadb748b816cb83f571c316c0787f51
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-02-23 12:45:50 +0100

    S2.01 Negatív oldalú négyzetet is példányosítani lehet!

--- 02-02-exception-debug/Program.cs
+++ 02-02-exception-debug/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
+using _02_02_exception_debug.model;
+
 namespace _02_02_exception_debug
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // S2.01 Negatív oldalú négyzetet is példányosítani lehet!
+            Square mySquare = new Square(-2);
+            Console.WriteLine(mySquare);
+            Console.WriteLine("Kerülete: "+mySquare.District+" méter.");
+            Console.WriteLine("Területe: "+mySquare.Area+" négyzetméter.");
         }
     }
 }
