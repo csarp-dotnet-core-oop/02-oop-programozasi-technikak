9c71bf799190145640af6f5cb61c4e2367ff1ce7
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-02-23 13:52:34 +0100

    Negatív oldalú négyzetet is példányosítani lehet kód kommentbe kerül.

--- 02-02-exception-debug/Program.cs
+++ 02-02-exception-debug/Program.cs
@@ -9,10 +9,10 @@ namespace _02_02_exception_debug
         static void Main(string[] args)
         {
             // S2.01 Negatív oldalú négyzetet is példányosítani lehet!
-            Square mySquare = new Square(-2);
-            Console.WriteLine(mySquare);
-            Console.WriteLine("Kerülete: "+mySquare.District+" méter.");
-            Console.WriteLine("Területe: "+mySquare.Area+" négyzetméter.");
+            // Square mySquare = new Square(-2);
+            // Console.WriteLine(mySquare);
+            // Console.WriteLine("Kerülete: "+mySquare.District+" méter.");
+            // Console.WriteLine("Területe: "+mySquare.Area+" négyzetméter.");
         }
     }
 }
