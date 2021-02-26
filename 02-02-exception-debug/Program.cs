4dec20298858a15a28704e02c37f955a4b591421
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-02-26 09:23:56 +0100

    S2.06 Ha negatív oldalú négyzetet példaányosítunk akkor elkapjuk a kivételt.

--- 02-02-exception-debug/Program.cs
+++ 02-02-exception-debug/Program.cs
@@ -13,6 +13,17 @@ namespace _02_02_exception_debug
             // Console.WriteLine(mySquare);
             // Console.WriteLine("Kerülete: "+mySquare.District+" méter.");
             // Console.WriteLine("Területe: "+mySquare.Area+" négyzetméter.");
-        }
-    }
+
+            // S2.06 Ha negatív oldalú négyzetet példaányosítunk akkor elkapjuk a kivételt.
+            //       A kivételkezelés egy programozási mechanizmus, melynek célja a program futását szándékosan
+            //       vagy nem szándékolt módon megszakító esemény (hiba) vagy utasítás kezelése.
+            //       https://hu.wikipedia.org/wiki/Kivételkezelés
+            //       Kivételkezelő blokk
+            //       A try blokk tartalmazza a kivételkezeléssel védett utasításokat.
+            //       https://csharptutorial.hu/docs/hellovilag-hellocsharp/4-vezerlesi-szerkezetek-es-blokkok/try-catch/
+            int wrongSide = -2;
+            try
+            {
+                Square myBadSquare = new Square(wrongSide);
+            }
 }
