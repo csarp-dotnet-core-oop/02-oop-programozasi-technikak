3c37686125856f365713a0a0377b1b7e1115b0c8
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-09-25 12:45:41 +0200

    A megírt kódok tesztelése

--- SquareProject/Program.cs
+++ SquareProject/Program.cs
@@ -3,3 +3,32 @@
 Példányosítson egy ilyen négyzetet és a főprogramban mutassa be a tulajdonságok kezelését!
 Az osztály a projekt model rétegében készítse el egy külön fájlba!
 */
+
+
+using SquareProject.Models;
+
+Square mySquare = new Square(2);
+Console.WriteLine(mySquare);
+Console.WriteLine("Kerülete: " + mySquare.Perimeter + " méter.");
+Console.WriteLine("Területe: " + mySquare.Area + " négyzetméter.");
+
+// A négyzet oldalát módosítani lehet
+mySquare.Side = 5;
+
+Console.WriteLine(mySquare);
+Console.WriteLine("Kerülete: " + mySquare.Perimeter + " méter.");
+Console.WriteLine("Területe: " + mySquare.Area + " négyzetméter.");
+
+// A konyhába és a fürdőszobába szeretnénk padló csempét
+Square myKitchen = new Square(4);
+Square myBathroom = new Square(5);
+// A négyzet oldalát módosítani lehet. Legyen nagyobb a konyha.
+myKitchen.Side = 6;
+double areaToBeTiled = myKitchen.Area + myBathroom.Area;
+
+Console.WriteLine();
+Console.WriteLine("Konyha, " + myKitchen);
+Console.WriteLine("Konyha területe: " + myKitchen.Area + " négyzetméter.");
+Console.WriteLine("Fürdőszoba, " + myBathroom);
+Console.WriteLine("Fürdőszoba területe: " + myBathroom.Area + " négyzetméter.");
+Console.WriteLine("Csempézve lesz: " + areaToBeTiled + " négyzetméter.");
