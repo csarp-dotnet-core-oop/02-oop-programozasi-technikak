8d330809a5ee1c3d1aac0ddb8d5dbebf126b4599
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-09-25 09:34:39 +0200

    A feladat szövege

--- SquareProject/Program.cs
+++ SquareProject/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ Vektorgrafikus rendszerekben lehetőség van négyzet rajzolására.  Az ilyen négyzetek méretét lehetséges változtatni. Az ilyen CAD rendszereknél fontos lehet a négyzet területének és kerületének meghatározása.  Készítsen olyan négyzet osztályt, amelynek adott az oldalhossza!  Az oldalhossz lekérhető és módosítható tulajdonsága legyen a négyzetnek!  A négyzet területe és kerülete legyen lekérdezhető tulajdonsága a négyzetnek!
+Példányosítson egy ilyen négyzetet és a főprogramban mutassa be a tulajdonságok kezelését!
+Az osztály a projekt model rétegében készítse el egy külön fájlba!
+*/
