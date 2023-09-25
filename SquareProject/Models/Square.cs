aa4816126f73a8decee3105295642ab38032c26b
Csaba Gyuris <gyuris.csaba@vasvari.org>
2023-09-25 09:50:28 +0200

    • Side tulajdonság get metódussal

--- SquareProject/Models/Square.cs
+++ SquareProject/Models/Square.cs
@@ -3,7 +3,11 @@
     public class Square
     {
         private double _side;
-        
+        public double Side
+        {
+            get => _side;
+        }
+
         public Square(double side)
         {
             _side = side;
